e8fe8b1f7b7c4ad4dbd0796199d770af6475bc00
Jorge Bejar <jorge@wyeworks.com>
2024-01-29 11:45:51 -0300

    Get rid of weird dpi_factor values in CachedView

--- src/shared/widgets.rs
+++ src/shared/widgets.rs
@@ -6,13 +6,11 @@ live_design! {
     import makepad_draw::shader::std::*;
 
     FadeView = <CachedView> {
-        dpi_factor: 2.0,
-        
         draw_bg: {
             instance opacity: 1.0
             
             fn pixel(self) -> vec4 {
-                let color = sample2d_rt(self.image, self.pos);
+                let color = sample2d_rt(self.image, self.pos * self.scale + self.shift) + vec4(self.marked, 0.0, 0.0, 0.0);
                 return Pal::premul(vec4(color.xyz, color.w * self.opacity))
             }
         }
