c26427935df7a0f020db5cd893efbbf8c4350370
Jorge Bejar <jorge@wyeworks.com>
2024-02-01 00:21:43 -0300

    Add background images to Artifacts carrousel

--- src/artifacts/artifacts_screen.rs
+++ src/artifacts/artifacts_screen.rs
@@ -9,21 +9,17 @@ live_design! {
 
     import crate::artifacts::artifacts_carrousel::*;
 
+    BACKGROUND_ITEM_COLOR = #333
+
     ArtifactsScreen = <View> {
         width: Fill, height: Fill
-        flow: Down,
-        spacing: 10.0,
+        flow: Overlay,
         align: {x: 0.5, y: 0.0},
 
-        show_bg: true,
-        draw_bg: {
-            color: #004
-        }
-
         <Label> {
-            margin: { top: 30.0 }
+            margin: { top: 40.0 }
             draw_text:{
-                text_style: <SUBTITLE_CAPTION>{font_size: 16},
+                text_style: <SUBTITLE_CAPTION>{font_size: 10},
                 color: #fff
             }
             text: "ARTIFACTS"
@@ -31,6 +27,31 @@ live_design! {
 
         <ArtifactsCarrousel> {
             margin: { top: 30.0 }
+        }
+
+        <View> {
+            height: Fill,
+            margin: 20,
+            flow: Down,
+            
+            <View> { height: Fill, width: 1 }
+
+            <Button> {
+                width: Fill,
+                height: 50,
+                text: "BROWSE ALL ARTIFACTS",
+                draw_text: {
+                    text_style: {
+                        font_size: 9.0
+                    }
+
+                    fn get_color(self) -> vec4 {
+                        return #fff
+                    }
+                }
+
+                draw_bg: { bodytop: (BACKGROUND_ITEM_COLOR), bodybottom: (BACKGROUND_ITEM_COLOR) }
+            }
         }
     }
 }
