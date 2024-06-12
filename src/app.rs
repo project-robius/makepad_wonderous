f271e511ca7bbd3d596cc89b3cfcbb0f2c95c0fd
Julián Montes de Oca <julian@wyeworks.com>
2024-06-12 14:42:44 -0300

    Navigation Bar Improvements (#33)
    * Rework navbar with proper icons
    
    * Add heads-up about CustomRadioButton
    
    * Fix tab background color

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,7 @@ use crate::{
         gallery_image_slider::GalleryImageSliderWidgetRefExt,
         gallery_screen::{GalleryGridAction, GalleryWidgetRefExt},
     },
+    shared::custom_radio_button::*,
     wonder::wonder_screen::{WonderScreenAction, WonderState},
 };
 use makepad_widgets::*;
@@ -10,6 +11,7 @@ use makepad_widgets::*;
 live_design! {
     import makepad_widgets::base::*;
     import makepad_widgets::theme_desktop_dark::*;
+    import makepad_draw::shader::std::*;
 
     import crate::shared::styles::*;
     import crate::wonder::wonder_screen::*;
@@ -18,30 +20,57 @@ live_design! {
     import crate::artifacts::artifacts_screen::*;
     import crate::timeline::timeline_screen::*;
     import crate::timeline::global_timeline::*;
+    import crate::shared::custom_radio_button::*;
 
-    ICON_WONDER = dep("crate://self/resources/icons/test.svg")
-    ICON_GALLERY = dep("crate://self/resources/icons/test.svg")
-    ICON_ARTIFACTS = dep("crate://self/resources/icons/test.svg")
-    ICON_TIMELINE = dep("crate://self/resources/icons/test.svg")
+    ICON_WONDER = dep("crate://self/resources/icons/wonder-button.png")
 
-    AppTab = <RadioButton> {
-        width: Fit,
+    ICON_MAIN = dep("crate://self/resources/icons/tab-editorial.png")
+    ICON_MAIN_ACTIVE = dep("crate://self/resources/icons/tab-editorial-active.png")
+    ICON_GALLERY = dep("crate://self/resources/icons/tab-photos.png")
+    ICON_GALLERY_ACTIVE = dep("crate://self/resources/icons/tab-photos-active.png")
+    ICON_ARTIFACTS = dep("crate://self/resources/icons/tab-artifacts.png")
+    ICON_ARTIFACTS_ACTIVE = dep("crate://self/resources/icons/tab-artifacts-active.png")
+
+    ICON_TIMELINE = dep("crate://self/resources/icons/tab-timeline.png")
+    ICON_TIMELINE_ACTIVE = dep("crate://self/resources/icons/tab-timeline-active.png")
+
+    AppTab = <CustomRadioButton> {
+        width: Fill,
         height: Fill,
         align: {x: 0.0, y: 0.0}
         draw_radio: {
             radio_type: Tab,
-            // TODO: Update radio button styling with current themeing
-            // color_active: #fff,
-            // color_inactive: #fff,
+            instance inset: vec4(0.0, 0.0, 0.0, 0.0)
+            instance radius: 2.5
+
+            fn pixel(self) -> vec4 {
+                return vec4(0.0, 0.0, 0.0, 0.0);
+            }
         }
-        draw_text: {
-            color_selected: #0b0,
-            color_unselected: #000,
-            color_unselected_hover: #111,
+        media: Image
+        image: <Image> {
+            width: 25,
+            height: Fit,
+            fit: Smallest
         }
+        flow: Down, spacing: 5.0, align: {x: 0.5, y: 0.5}
     }
 
     App = {{App}} {
+        menu_images: [
+            (ICON_MAIN),
+            (ICON_GALLERY),
+            (ICON_ARTIFACTS),
+            (ICON_TIMELINE),
+        ]
+
+        menu_images_active: [
+            (ICON_MAIN_ACTIVE),
+            (ICON_GALLERY_ACTIVE),
+            (ICON_ARTIFACTS_ACTIVE),
+            (ICON_TIMELINE_ACTIVE),
+        ]
+
         ui: <Window> {
             window: {position: vec2(0, 0), inner_size: vec2(375, 813)},
             pass: {clear_color: #2A}
@@ -52,6 +81,7 @@ live_design! {
                         width: Fill,
                         height: Fill,
                         padding: 0, align: {x: 0.0, y: 0.0}, spacing: 0., flow: Down
+                        show_bg: false
 
                         application_pages = <View> {
                             margin: 0.0,
@@ -73,66 +103,54 @@ live_design! {
                                 color: #fff
                             }
                             mobile_modes = <View> {
-                                tab1 = <AppTab> {
+                                padding: {left: 10.0, right: 10.0}
+                                tab_wonder = <AppTab> {
                                     animator: {selected = {default: on}}
-                                    draw_icon: {
-                                        svg_file: (ICON_WONDER),
-                                        fn get_color(self) -> vec4 {
-                                            return mix(
-                                                #000,
-                                                #e6945c,
-                                                self.selected
-                                            )
+                                    image: {
+                                        source: (ICON_WONDER)
+                                        width: 50,
+                                        draw_bg: {
+                                            fn get_color(self) -> vec4 {
+                                                let color = self.get_color_scale_pan(self.image_scale, self.image_pan);
+
+                                                if color.a < 0.01 {
+                                                    return #678551;
+                                                }
+
+                                                return color;
+                                            }
+
+                                            fn pixel(self) -> vec4 {
+                                                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                                                let c = self.rect_size * 0.5;
+                                                sdf.circle(c.x, c.y, c.x - 2.)
+                                                sdf.fill_keep(self.get_color());
+                                                sdf.stroke(#xfff8ee, 1);
+                                                return sdf.result
+                                            }
                                         }
                                     }
-                                    width: Fill,
-                                    icon_walk: {width: 20, height: 20}
-                                    flow: Down, spacing: 5.0, align: {x: 0.5, y: 0.5}
                                 }
-                                tab2 = <AppTab> {
-                                    draw_icon: {
-                                        svg_file: (ICON_GALLERY),
-                                        fn get_color(self) -> vec4 {
-                                            return mix(
-                                                #000,
-                                                #e6945c,
-                                                self.selected
-                                            )
-                                        }
+                                tab_main = <AppTab> {
+                                    animator: {selected = {default: on}}
+                                    image: {
+                                        source: (ICON_MAIN_ACTIVE)
                                     }
-                                    width: Fill
-                                    icon_walk: {width: 20, height: 20}
-                                    flow: Down, spacing: 5.0, align: {x: 0.5, y: 0.5}
                                 }
-                                tab3 = <AppTab> {
-                                    draw_icon: {
-                                        svg_file: (ICON_ARTIFACTS),
-                                        fn get_color(self) -> vec4 {
-                                            return mix(
-                                                #000,
-                                                #e6945c,
-                                                self.selected
-                                            )
-                                        }
+                                tab_gallery = <AppTab> {
+                                    image: {
+                                        source: (ICON_GALLERY)
                                     }
-                                    width: Fill
-                                    icon_walk: {width: 20, height: 20}
-                                    flow: Down, spacing: 5.0, align: {x: 0.5, y: 0.5}
                                 }
-                                tab4 = <AppTab> {
-                                    draw_icon: {
-                                        svg_file: (ICON_TIMELINE),
-                                        fn get_color(self) -> vec4 {
-                                            return mix(
-                                                #000,
-                                                #e6945c,
-                                                self.selected
-                                            )
-                                        }
+                                tab_artifacts = <AppTab> {
+                                    image: {
+                                        source: (ICON_ARTIFACTS)
                                     }
-                                    width: Fill
-                                    icon_walk: {width: 20, height: 20}
-                                    flow: Down, spacing: 5.0, align: {x: 0.5, y: 0.5}
+                                }
+                                tab_timeline = <AppTab> {
+                                    image: {
+                                        source: (ICON_TIMELINE)
+                                    }
                                 }
                             }
                         }
@@ -151,7 +169,7 @@ live_design! {
                                     title = {
                                     // Hack to make sure the button is visible, we render some text in the same
                                     // color as the background
-                                        text: "." 
+                                        text: "."
                                         draw_text: {
                                             fn get_color(self) -> vec4 {
                                                 return #1f1b18;
@@ -212,6 +230,10 @@ app_main!(App);
 pub struct App {
     #[live]
     ui: WidgetRef,
+    #[live]
+    menu_images: Vec<LiveDependency>,
+    #[live]
+    menu_images_active: Vec<LiveDependency>,
 }
 
 impl LiveRegister for App {
@@ -222,6 +244,7 @@ impl LiveRegister for App {
         crate::shared::styles::live_design(cx);
         crate::shared::widgets::live_design(cx);
         crate::shared::curved_label::live_design(cx);
+        crate::shared::custom_radio_button::live_design(cx);
         crate::shared::helpers::live_design(cx);
 
         // Wonder
@@ -254,11 +277,11 @@ impl LiveRegister for App {
 impl MatchEvent for App {
     fn handle_actions(&mut self, cx: &mut Cx, actions: &Actions) {
         self.ui
-            .radio_button_set(ids!(
-                mobile_modes.tab1,
-                mobile_modes.tab2,
-                mobile_modes.tab3,
-                mobile_modes.tab4,
+            .custom_radio_button_set(ids!(
+                mobile_modes.tab_main,
+                mobile_modes.tab_gallery,
+                mobile_modes.tab_artifacts,
+                mobile_modes.tab_timeline,
             ))
             .selected_to_visible(
                 cx,
@@ -271,6 +294,31 @@ impl MatchEvent for App {
                     application_pages.tab4_frame,
                 ),
             );
+
+        let selected_tab = self
+            .ui
+            .custom_radio_button_set(ids!(
+                mobile_modes.tab_main,
+                mobile_modes.tab_gallery,
+                mobile_modes.tab_artifacts,
+                mobile_modes.tab_timeline,
+            ))
+            .selected(cx, actions);
+
+        if let Some(tab_index) = selected_tab {
+            let tab_ref = self.tab_ref_from_index(tab_index);
+            let _ = tab_ref.load_image_dep_by_path(cx, self.menu_images_active[tab_index].as_str());
+
+            for inactive_tab in 0..self.menu_images.len() {
+                if inactive_tab != tab_index {
+                    let tab_ref = self.tab_ref_from_index(inactive_tab);
+                    let _ =
+                        tab_ref.load_image_dep_by_path(cx, self.menu_images[inactive_tab].as_str());
+                }
+            }
+
+            self.ui.redraw(cx);
+        }
 
         let mut navigation = self.ui.stack_navigation(id!(navigation));
         navigation.handle_stack_view_actions(cx, &actions);
@@ -291,6 +339,16 @@ impl AppMain for App {
 }
 
 impl App {
+    fn tab_ref_from_index(&self, tab_index: usize) -> CustomRadioButtonRef {
+        match tab_index {
+            0 => self.ui.custom_radio_button(id!(mobile_modes.tab_main)),
+            1 => self.ui.custom_radio_button(id!(mobile_modes.tab_gallery)),
+            2 => self.ui.custom_radio_button(id!(mobile_modes.tab_artifacts)),
+            3 => self.ui.custom_radio_button(id!(mobile_modes.tab_timeline)),
+            _ => unreachable!(),
+        }
+    }
+
     fn handle_mobile_menu_visibility(&mut self, actions: &Actions) {
         // hide menu on first page
         let stack_navigation = self.ui.stack_navigation(id!(navigation));
---
+++ src/shared/custom_radio_button.rs
@@ -0,0 +1,426 @@
+use makepad_widgets::{
+    image_cache::ImageCacheImpl, makepad_derive_widget::*, makepad_draw::*, widget::*, Image, View
+};
+
+// FIXME: This customized clone of RadioButton is a workaround for Makepad's RadioButton not exposing ImageRef
+// (therefore we can't update the images programmatically from the outside of the widget)
+// A proper solution will be to expose inner widgets from WidgetRefs in Makepad itself.
+
+live_design! {
+    import makepad_widgets::base::*;
+    import makepad_widgets::theme_desktop_dark::*;
+    import makepad_draw::shader::std::*;
+
+DrawRadioButton = {{DrawRadioButton}} {}
+CustomRadioButton = {{CustomRadioButton}}  {
+    // TODO: adda  focus states
+    width: Fit, height: 16.,
+    align: { x: 0.0, y: 0.5 }
+
+    icon_walk: { margin: { left: 20. } }
+
+       label_walk: {
+            width: Fit, height: Fit,
+        margin: { left: 20. }
+    }
+    label_align: { y: 0.0 }
+
+    draw_radio: {
+        uniform size: 7.0;
+        // uniform color_active: (THEME_COLOR_U_2)
+        // uniform color_inactive: (THEME_COLOR_D_4)
+
+        // instance pressed: 0.0
+        uniform border_radius: (THEME_CORNER_RADIUS)
+        instance bodytop: (THEME_COLOR_CTRL_DEFAULT)
+        instance bodybottom: (THEME_COLOR_CTRL_ACTIVE)
+
+        fn pixel(self) -> vec4 {
+            let sdf = Sdf2d::viewport(self.pos * self.rect_size)
+            match self.radio_type {
+                RadioType::Round => {
+                    let sz = self.size;
+                    let left = sz + 1.;
+                    let c = vec2(left + sz, self.rect_size.y * 0.5);
+                    sdf.circle(left, c.y, sz);
+                    sdf.fill_keep(mix(THEME_COLOR_INSET_PIT_TOP, THEME_COLOR_INSET_PIT_BOTTOM, pow(self.pos.y, 1.)))
+                    sdf.stroke(mix(THEME_COLOR_BEVEL_SHADOW, THEME_COLOR_BEVEL_LIGHT, self.pos.y), (THEME_BEVELING))
+                    let isz = sz * 0.5;
+                    sdf.circle(left, c.y, isz);
+                    sdf.fill(
+                        mix(
+                            mix(
+                                THEME_COLOR_U_HIDDEN,
+                                THEME_COLOR_CTRL_HOVER,
+                                self.hover
+                            ),
+                            THEME_COLOR_TEXT_ACTIVE,
+                            self.selected
+                        )
+                    );
+                }
+                RadioType::Tab => {
+                    let grad_top = 5.0;
+                    let grad_bot = 1.0;
+                    let body = mix(
+                        mix(self.bodytop, (THEME_COLOR_CTRL_HOVER), self.hover),
+                        self.bodybottom,
+                        self.selected
+                    );
+                    let body_transp = vec4(body.xyz, 0.0);
+                    let top_gradient = mix(body_transp, mix(THEME_COLOR_BEVEL_LIGHT, THEME_COLOR_BEVEL_SHADOW, self.selected), max(0.0, grad_top - sdf.pos.y) / grad_top);
+                    let bot_gradient = mix(
+                        mix(body_transp, THEME_COLOR_BEVEL_LIGHT, self.selected),
+                        top_gradient,
+                        clamp((self.rect_size.y - grad_bot - sdf.pos.y - 1.0) / grad_bot, 0.0, 1.0)
+                    );
+
+                    // the little drop shadow at the bottom
+                    let shift_inward = 0. * 1.75;
+                    sdf.move_to(shift_inward, self.rect_size.y);
+                    sdf.line_to(self.rect_size.x - shift_inward, self.rect_size.y);
+                    sdf.stroke(
+                        mix(
+                            THEME_COLOR_BEVEL_SHADOW,
+                            THEME_COLOR_U_HIDDEN,
+                            self.selected
+                        ), THEME_BEVELING * 2.)
+
+                    sdf.box(
+                        1.,
+                        1.,
+                        self.rect_size.x - 2.0,
+                        self.rect_size.y - 2.0,
+                        1.
+                    )
+                    sdf.fill_keep(body)
+
+                    sdf.stroke(bot_gradient, THEME_BEVELING * 1.5)
+                }
+            }
+            return sdf.result
+        }
+    }
+
+    draw_text: {
+        instance hover: 0.0
+        instance selected: 0.0
+
+        uniform color_unselected: (THEME_COLOR_TEXT_DEFAULT)
+        uniform color_unselected_hover: (THEME_COLOR_TEXT_HOVER)
+        uniform color_selected: (THEME_COLOR_TEXT_SELECTED)
+
+        text_style: <THEME_FONT_REGULAR> {
+            font_size: (THEME_FONT_SIZE_P)
+        }
+        fn get_color(self) -> vec4 {
+            return mix(
+                mix(
+                    self.color_unselected,
+                    self.color_unselected,
+                    // self.color_unselected_hover,
+                    self.hover
+                ),
+                self.color_unselected,
+                // self.color_selected,
+                self.selected
+            )
+        }
+    }
+
+    draw_icon: {
+        instance hover: 0.0
+        instance selected: 0.0
+        uniform color: (THEME_COLOR_INSET_PIT_TOP)
+        uniform color_active: (THEME_COLOR_TEXT_ACTIVE)
+        fn get_color(self) -> vec4 {
+            return mix(
+                mix(
+                    self.color,
+                    mix(self.color, #f, 0.4),
+                    self.hover
+                ),
+                mix(
+                    self.color_active,
+                    mix(self.color_active, #f, 0.75),
+                    self.hover
+                ),
+                self.selected
+            )
+        }
+    }
+
+    animator: {
+        hover = {
+            default: off
+            off = {
+                from: {all: Forward {duration: 0.15}}
+                apply: {
+                    draw_radio: {hover: 0.0}
+                    draw_text: {hover: 0.0}
+                    draw_icon: {hover: 0.0}
+                }
+            }
+            on = {
+                from: {all: Snap}
+                apply: {
+                    draw_radio: {hover: 1.0}
+                    draw_text: {hover: 1.0}
+                    draw_icon: {hover: 1.0}
+                }
+            }
+        }
+        selected = {
+            default: off
+            off = {
+                from: {all: Forward {duration: 0.2}}
+                apply: {
+                    draw_radio: {selected: 0.0}
+                    draw_icon: {selected: 0.0}
+                    draw_text: {selected: 0.0}
+                    draw_icon: {selected: 0.0}
+                }
+            }
+            on = {
+                cursor: Arrow,
+                from: {all: Forward {duration: 0.0}}
+                apply: {
+                    draw_radio: {selected: 1.0}
+                    draw_icon: {selected: 1.0}
+                    draw_text: {selected: 1.0}
+                    draw_icon: {selected: 1.0}
+                }
+            }
+        }
+    }
+}
+RadioButtonGroupBase = {{RadioButtonGroup }} {}
+}
+
+#[derive(Live, LiveHook, LiveRegister)]
+#[repr(C)]
+pub struct DrawRadioButton {
+    #[deref]
+    draw_super: DrawQuad,
+    #[live]
+    radio_type: RadioType,
+    #[live]
+    hover: f32,
+    #[live]
+    focus: f32,
+    #[live]
+    selected: f32,
+}
+
+#[derive(Live, LiveHook)]
+#[live_ignore]
+#[repr(u32)]
+pub enum RadioType {
+    #[pick]
+    Round = shader_enum(1),
+    Tab = shader_enum(2),
+}
+
+#[derive(Live, LiveHook)]
+#[live_ignore]
+pub enum MediaType {
+    Image,
+    #[pick]
+    Icon,
+    None,
+}
+
+#[derive(Live, LiveHook, Widget)]
+pub struct RadioButtonGroup {
+    #[deref]
+    frame: View,
+}
+
+#[derive(Live, LiveHook, Widget)]
+pub struct CustomRadioButton {
+    #[redraw]
+    #[live]
+    draw_radio: DrawRadioButton,
+    #[live]
+    draw_icon: DrawIcon,
+    #[live]
+    draw_text: DrawText,
+
+    #[live]
+    value: LiveValue,
+
+    #[live]
+    media: MediaType,
+
+    #[live]
+    icon_walk: Walk,
+    #[walk]
+    walk: Walk,
+
+    #[live]
+    image: Image,
+
+    #[layout]
+    layout: Layout,
+    #[animator]
+    animator: Animator,
+
+    #[live]
+    label_walk: Walk,
+    #[live]
+    label_align: Align,
+    #[live]
+    text: RcStringMut,
+
+    #[live]
+    bind: String,
+}
+
+#[derive(Clone, Debug, DefaultNone)]
+pub enum RadioButtonAction {
+    Clicked,
+    None,
+}
+
+impl RadioButtonGroup {
+    pub fn draw_walk(&mut self, _cx: &mut Cx2d, _walk: Walk) {}
+}
+
+impl CustomRadioButton {
+    pub fn draw_walk(&mut self, cx: &mut Cx2d, walk: Walk) {
+        self.draw_radio.begin(cx, walk, self.layout);
+        match self.media {
+            MediaType::Image => {
+                let image_walk = self.image.walk(cx);
+                let _ = self.image.draw_walk(cx, image_walk);
+            }
+            MediaType::Icon => {
+                self.draw_icon.draw_walk(cx, self.icon_walk);
+            }
+            MediaType::None => {}
+        }
+        self.draw_text
+            .draw_walk(cx, self.label_walk, self.label_align, self.text.as_ref());
+        self.draw_radio.end(cx);
+    }
+}
+
+impl Widget for RadioButtonGroup {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event, _scope: &mut Scope) {
+        //let uid = self.widget_uid();
+        self.animator_handle_event(cx, event);
+    }
+
+    fn draw_walk(&mut self, cx: &mut Cx2d, _scope: &mut Scope, walk: Walk) -> DrawStep {
+        self.draw_walk(cx, walk);
+        DrawStep::done()
+    }
+}
+
+impl Widget for CustomRadioButton {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
+        let uid = self.widget_uid();
+        self.animator_handle_event(cx, event);
+
+        match event.hits(cx, self.draw_radio.area()) {
+            Hit::FingerHoverIn(_) => {
+                cx.set_cursor(MouseCursor::Hand);
+                self.animator_play(cx, id!(hover.on));
+            }
+            Hit::FingerHoverOut(_) => {
+                cx.set_cursor(MouseCursor::Arrow);
+                self.animator_play(cx, id!(hover.off));
+            }
+            Hit::FingerDown(_fe) => {
+                if self.animator_in_state(cx, id!(selected.off)) {
+                    self.animator_play(cx, id!(selected.on));
+                    cx.widget_action(uid, &scope.path, RadioButtonAction::Clicked);
+                }
+            }
+            Hit::FingerUp(_fe) => {}
+            Hit::FingerMove(_fe) => {}
+            _ => (),
+        }
+    }
+
+    fn draw_walk(&mut self, cx: &mut Cx2d, _scope: &mut Scope, walk: Walk) -> DrawStep {
+        self.draw_walk(cx, walk);
+        DrawStep::done()
+    }
+
+    fn text(&self) -> String {
+        self.text.as_ref().to_string()
+    }
+
+    fn set_text(&mut self, v: &str) {
+        self.text.as_mut_empty().push_str(v);
+    }
+}
+
+impl CustomRadioButtonRef {
+    fn unselect(&self, cx: &mut Cx) {
+        if let Some(mut inner) = self.borrow_mut() {
+            inner.animator_play(cx, id!(selected.off));
+        }
+    }
+
+    pub fn _select(&self, cx: &mut Cx, scope: &mut Scope) {
+        if let Some(mut inner) = self.borrow_mut() {
+            if inner.animator_in_state(cx, id!(selected.off)) {
+                inner.animator_play(cx, id!(selected.on));
+                cx.widget_action(inner.widget_uid(), &scope.path, RadioButtonAction::Clicked);
+            }
+        }
+    }
+
+    pub fn load_image_dep_by_path(&self, cx: &mut Cx, image_path: &str) {
+        if let Some(mut inner) = self.borrow_mut() {
+            let _ = inner.image.load_image_dep_by_path(cx, image_path, 0);
+        }
+    }
+}
+
+impl CustomRadioButtonSet {
+    pub fn selected(&self, cx: &mut Cx, actions: &Actions) -> Option<usize> {
+        for action in actions {
+            if let Some(action) = action.as_widget_action() {
+                match action.cast() {
+                    RadioButtonAction::Clicked => {
+                        if let Some(index) = self
+                            .0
+                            .iter()
+                            .position(|v| action.widget_uid == v.widget_uid())
+                        {
+                            for (i, item) in self.0.iter().enumerate() {
+                                if i != index {
+                                    CustomRadioButtonRef(item).unselect(cx);
+                                }
+                            }
+                            return Some(index);
+                        }
+                    }
+                    _ => (),
+                }
+            }
+        }
+        None
+    }
+
+    pub fn selected_to_visible(
+        &self,
+        cx: &mut Cx,
+        ui: &WidgetRef,
+        actions: &Actions,
+        paths: &[&[LiveId]],
+    ) {
+        // find a widget action that is in our radiogroup
+        if let Some(index) = self.selected(cx, actions) {
+            // ok now we set visible
+            for (i, path) in paths.iter().enumerate() {
+                let widget = ui.widget(path);
+                widget.apply_over(cx, live! {visible:(i == index)});
+                widget.redraw(cx);
+            }
+        }
+    }
+}
